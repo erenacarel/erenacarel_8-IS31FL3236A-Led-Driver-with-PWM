18b24ead16be94215c72ce4924f59a9ce9c383c5
erenacarel <93088715+erenacarel@users.noreply.github.com>
2023-08-14 12:33:20 +0300

    Add files via upload

---
+++ Button_Debounce_F407/Core/Inc/usr_general.h
@@ -0,0 +1,20 @@
+#ifndef __USR_GENERAL_H
+#define __USR_GENERAL_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h> 
+#include <math.h>
+
+
+#define _io  static
+#define _iov static volatile
+
+#include "tim.h"
+#include "gpio.h"
+
+#endif
+
+
